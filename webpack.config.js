f7df6fefe18e267e94867476f14d733a72d1cf67
Chester Troy Baluyo <chester.baluyo@gmail.com>
2017-11-12 21:50:37 +0800

    Generate index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/app.js',
     output: {
         filename: 'app.bundle.js',
         path: path.resolve(__dirname, 'dist')
-    }
-}
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/main.html',
+            filename: '../index.html',
+            hash: true,
+            minify: {
+                collapseWhitespace: true
+            }
+        })
+    ]
+};
