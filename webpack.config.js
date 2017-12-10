0afad91e8f47e9480867d017b0040f914ad0877b
Chester Troy Baluyo <chester.baluyo@gmail.com>
2017-12-10 20:06:09 +0800

    Configure bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const webpack = require('webpack'); //to access built-in plugins
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
@@ -21,6 +22,25 @@ module.exports = {
                         {loader: 'css-loader'}
                     ]
                 })
+            }, {
+                test: /\.(scss)$/,
+                use: [{
+                        loader: 'style-loader', // inject CSS to page
+                    }, {
+                        loader: 'css-loader', // translates CSS into CommonJS modules
+                    }, {
+                        loader: 'postcss-loader', // Run post css actions
+                        options: {
+                            plugins: function () { // post css plugins, can be exported to postcss.config.js
+                                return [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ];
+                            }
+                        }
+                    }, {
+                        loader: 'sass-loader' // compiles SASS to CSS
+                    }]
             }
         ]
     },
@@ -36,6 +56,12 @@ module.exports = {
         }),
         new HtmlWebpackHarddiskPlugin({
             outputPath: path.resolve(__dirname, '')
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            Popper: ['popper.js', 'default']
         })
     ],
     devServer: {
