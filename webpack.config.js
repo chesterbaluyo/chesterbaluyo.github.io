5bb33002e1ecbe9b5dfac404dfbaae1fe5d316ad
Chester Troy Baluyo <chester.baluyo@gmail.com>
2018-01-16 12:37:16 +0800

    Add null checking of environment variable

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const prodConfig = require('./config/webpack.prod');
 
 
 module.exports = env => {
-    if (env.production) {
+    if (env && env.production) {
         return prodConfig;
     } else {
         return devConfig;
